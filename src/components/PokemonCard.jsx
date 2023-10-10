1c4fa4d1ddeea0ae723deba672c60f41a5a15158
vincentlouvart82@gmail.com <Vinc441>
2023-10-10 10:23:07 +0200

    components.2

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,11 +1,36 @@
 
-function App() {
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
 
-    return <figure>
-<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"></img>
-<figcaption>Bulbasaur</figcaption>
+
+function PokemonCard() {
+
+    const pokemon = pokemonList[0];
+
+    return (
+    <>
+    <figure>
+        
+        {pokemon.imgSrc === undefined ? <p>???</p>:
+<img src={pokemon.imgSrc} />}
+
 </figure>
-    
+
+<figcaption>{pokemon.name}</figcaption>
+
+</>
+    );
 }
 
-export default App
+
+
+export default PokemonCard
+
