ba56b338c20bd01150c59146c1be7c0c13b6733b
vincentlouvart82@gmail.com <Vinc441>
2023-10-11 11:24:49 +0200

    state.1 + button

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,66 @@
 import PokemonCard from "./components/PokemonCard"
+import { useState } from "react";
 
 
-
-
-function App() {
-  
-  const pokemonList = [
-    {
+const pokemonList = [
+  {
       name: "bulbasaur",
       imgSrc:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
     },
     {
       name: "mew",
     },
   ];
 
+
+function App() {
+  
+const [pokemonIndex, setpokemonIndex] = useState(0);
+console.log(pokemonIndex)
+
+
+
+
   
   return (
+ 
+
 <div>
-<PokemonCard pokemon = {pokemonList[0]} />
+
+{pokemonIndex > 0 && (
+         
+<button onClick ={() => setpokemonIndex(pokemonIndex - 1)}>
+  Précedent</button>
+   )}
+
+
+{ pokemonIndex < pokemonList.length - 1 && (
+
+<button onClick ={() => setpokemonIndex(pokemonIndex + 1)}>
+  Suivant</button>
+
+)}
+
+<PokemonCard pokemon = {pokemonList[pokemonIndex]} />
 </div>
   );
 }
-
 
 
 export default App
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -3,22 +3,13 @@ import PropTypes from "prop-types";
 
 
 
-const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
-
 
 
 function PokemonCard(props) {
 console.log(props)
     const pokemon = props.pokemon;
+
+
 
     return (
     <>
